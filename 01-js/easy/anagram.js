934c89c6c9bd77359c6d671738c09ab48f170ca5
pashupat <tushareshukla@gmail.com>
2024-04-24 04:43:12 +0530

    week-01-anagram.js

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,7 +5,23 @@
 */
 
 function isAnagram(str1, str2) {
-
+    // Remove non-alphanumeric characters and convert to lowercase
+    const cleanStr1 = str1.replace(/[^\w!]/g, '').toLowerCase();
+    const cleanStr2 = str2.replace(/[^\w!]/g, '').toLowerCase();
+    
+    // Check if the lengths of the cleaned strings are different
+    if (cleanStr1.length !== cleanStr2.length) {
+        return false;
+    }
+    
+    // Sort the characters in the strings
+    const sortedStr1 = cleanStr1.split('').sort().join('');
+    const sortedStr2 = cleanStr2.split('').sort().join('');
+    
+    // Compare the sorted strings
+    return sortedStr1 === sortedStr2;
 }
+
+// isAnagram()
 
 module.exports = isAnagram;
